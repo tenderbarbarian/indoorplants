a5ee39d63d5b8fbd63eb6833fb4aa3d3cae58529
Kasia Pohl <ksoloduc@gmail.com>
2020-02-17 11:06:13 +0100

    updated model

---
+++ models/plant.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const plantSchema = new mongoose.Schema({
+	name: String,
+	price: String,
+	image: String,
+	description: String,
+	comments: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Comment'
+		}
+	],
+	author: {
+		id: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		},
+		username: String
+	}
+});
+
+//compiling schema into a model
+module.exports = mongoose.model('Plant', plantSchema);
