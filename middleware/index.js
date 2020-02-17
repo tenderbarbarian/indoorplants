583c19971925ccfc32bad42ae51154978973c9cc
Kasia Pohl <ksoloduc@gmail.com>
2020-02-17 09:31:11 +0100

    update to year 2020

--- middleware/index.js
+++ middleware/index.js
@@ -1,55 +1,53 @@
 //const passport = require('passport');
 const middlewareOBJ = {};
 
-middlewareOBJ.isLoggedIn = function(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
-  }
-  req.flash('error', 'You need to be logged in to do that.');
-  res.redirect('/login');
+middlewareOBJ.isLoggedIn = function(req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	req.flash('error', 'You need to be logged in to do that.');
+	res.redirect('/login');
 };
 
-middlewareOBJ.checkCommentOwnership = function(req, res, next){
-  console.log(req);
-  if(req.isAuthenticated()){
-    Comment.findById(req.params.comment_id)
-        .then((foundComment)=>{
-          if(foundComment.author.id.equals(req.user._id)){
-            return next();
-          }
-          console.log('eeeeee1');
-          res.redirect('back');
-        })
-        .catch((err)=>{
-          console.log('eeeeeeeeeee2');
-          res.redirect('back');
-        });
-  }else{
-    req.flash('error', 'You are not authorised to do that.');
-    res.redirect('back');
-  }
-}
+middlewareOBJ.checkCommentOwnership = function(req, res, next) {
+	console.log(req);
+	if (req.isAuthenticated()) {
+		Comment.findById(req.params.comment_id)
+			.then((foundComment) => {
+				if (foundComment.author.id.equals(req.user._id)) {
+					return next();
+				}
+				res.redirect('back');
+			})
+			.catch((err) => {
+				res.redirect('back');
+			});
+	} else {
+		req.flash('error', 'You are not authorised to do that.');
+		res.redirect('back');
+	}
+};
 
-middlewareOBJ.checkCampgroundOwnership = function(req, res, next){
-  if(req.isAuthenticated()){
-    Campground.findById(req.params.id)
-        .then((campground)=>{
-          //does user own the campground?
-          if(campground.author.id.equals(req.user._id)){
-            next();
-          } else{
-            req.flash('error','You are not authorised to do that.');
-            res.redirect('back');
-          }
-        })
-        .catch((err)=>{
-          req.flash('error', "Campground not found.");
-          res.redirect('back');
-        });
-  }else{
-    req.flash('error','You are not authorised to do that.');
-    res.redirect('back');
-  }
-}
+middlewareOBJ.checkCampgroundOwnership = function(req, res, next) {
+	if (req.isAuthenticated()) {
+		Campground.findById(req.params.id)
+			.then((campground) => {
+				//does user own the campground?
+				if (campground.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					req.flash('error', 'You are not authorised to do that.');
+					res.redirect('back');
+				}
+			})
+			.catch((err) => {
+				req.flash('error', 'Campground not found.');
+				res.redirect('back');
+			});
+	} else {
+		req.flash('error', 'You are not authorised to do that.');
+		res.redirect('back');
+	}
+};
 
 module.exports = middlewareOBJ;
